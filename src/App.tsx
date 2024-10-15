9b03b27abfa2669bedcd695eae2b63e89a1160cc
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-15 20:14:13 +0900

    update:ヘッダー追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PlaceholderPage from './PlaceholderPage';
 import LoginPage from './components/Login/LoginPage';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/placeholder" element={<PlaceholderPage />} />
         <Route path="/login" element={<LoginPage />} />
