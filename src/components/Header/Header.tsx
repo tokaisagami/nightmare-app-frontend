11151ae0a5391cafe11da3abb6d9dd7a4ee75b1c
tokaisagami <tokai.sagami.54@gmail.com>
2024-10-17 00:07:13 +0900

    update:CSS適用

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,3 @@
-// src/components/Header/Header.tsx
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../store/store';
@@ -9,20 +8,20 @@ const Header = () => {
   const dispatch = useDispatch();
 
   return (
-    <header className="bg-gray-800 text-white p-4">
-      <h1 className="text-xl">My App</h1>
+    <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
+      <h1 className="text-xl font-bold">My App</h1>
       {isLoggedIn ? (
         <nav>
-          <ul>
-            <li><a href="/dashboard">Dashboard</a></li>
-            <li><a href="/logout" onClick={() => dispatch(logout())}>Logout</a></li>
+          <ul className="flex space-x-4">
+            <li><a className="hover:text-gray-400" href="/dashboard">Dashboard</a></li>
+            <li><a className="hover:text-gray-400" href="/logout" onClick={() => dispatch(logout())}>Logout</a></li>
           </ul>
         </nav>
       ) : (
         <nav>
-          <ul>
-            <li><a href="/login">Login</a></li>
-            <li><a href="/signup">Signup</a></li>
+          <ul className="flex space-x-4">
+            <li><a className="hover:text-gray-400" href="/login">Login</a></li>
+            <li><a className="hover:text-gray-400" href="/signup">Signup</a></li>
           </ul>
         </nav>
       )}
