ca7f1205eff350d1a6c8d9ce2e2af12c48d66807
tokaisagami <tokai.sagami.54@gmail.com>
2024-11-04 23:04:41 +0900

    update:トークンのペイロードからユーザーIDを取得し、バックエンドからユーザー情報を取得する処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,19 @@ import MainPage from './components/MainPage/MainPage';
 import NightmareDetail from './components/MainPage/NightmareDetail';
 import InputNightmare from './components/MainPage/InputNightmare';
 import DisplayNightmare from './components/MainPage/DisplayNightmare';
-import HomePage from './components/HomePage/HomePage'; // 新規トップページをインポート
+import HomePage from './components/HomePage/HomePage';
+import MyPage from './components/MyPage/MyPage';
 import { login } from './store/slices/authSlice';
 
 const routes = [
-  { path: '/', element: <HomePage /> }, // トップページを追加
+  { path: '/', element: <HomePage /> }, 
   { path: '/login', element: <LoginPage /> },
   { path: '/signup', element: <UserSignupPage /> },
   { path: '/mainPage', element: <MainPage /> },
   { path: '/nightmares/:id', element: <NightmareDetail /> },
   { path: '/input-nightmare', element: <InputNightmare /> },
   { path: '/modified-nightmare', element: <DisplayNightmare /> },
+  { path: '/mypage', element: <MyPage /> },
 ];
 
 function App() {
@@ -26,10 +28,31 @@ function App() {
   useEffect(() => {
     const token = localStorage.getItem('authToken');
     if (token) {
-      dispatch(login()); // トークンが存在する場合にログイン状態を更新
+      const base64Url = token.split('.')[1];
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+      const payload = JSON.parse(jsonPayload);
+      const userId = payload.user_id; // トークンのペイロードからユーザーIDを取得
+  
+      // バックエンドからユーザー情報を取得
+      fetch(`${import.meta.env.VITE_APP_API_URL}/api/v1/users/${userId}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          const user = { name: data.name, email: data.email };
+          console.log(user); // ここでユーザー情報を確認
+          dispatch(login(user)); // ユーザー情報を渡してログイン状態を更新
+        })
+        .catch(error => console.error('Error fetching user data:', error));
     }
   }, [dispatch]);
-
+    
   return (
     <Router>
       <div className="min-h-screen">
