c377a47abb9e408cf0651bfcb30bb696b4e45d48
tokaisagami <tokai.sagami.54@gmail.com>
2024-11-09 02:43:36 +0900

    add&update:結末によって悪夢一覧のサムネイル表示を返る処理を追加実装

--- src/components/MainPage/PostCard/PostCard.tsx
+++ src/components/MainPage/PostCard/PostCard.tsx
@@ -8,6 +8,12 @@ interface PostCardProps {
 }
 
 const PostCard: React.FC<PostCardProps> = ({ description, modified_description, author, ending_category }) => {
+  // サムネイル画像の決定
+  const endingImage = {
+    happy_end: 'src/assets/nightmare-card/HAPPY-END.png',
+    unexpected_end: 'src/assets/nightmare-card/unexpected-end.png'
+  }[ending_category];
+
   // ending_categoryに基づく表示内容を決定
   const endingText = {
     happy_end: 'ハッピーエンド',
@@ -17,11 +23,9 @@ const PostCard: React.FC<PostCardProps> = ({ description, modified_description, 
 
   return (
     <div className="post-card p-4 mb-4 border rounded-xl shadow-lg bg-white h-full">
-      <h3 className="text-2xl md:text-3xl lg:text-2xl font-semibold text-center mb-4">{author}さんの悪夢</h3>
-      <h3 className="text-lg font-semibold mb-1">悪夢内容：</h3>
-      <p className="border-l-4 border-rose-300 pl-4 text-left mb-4 break-words">{description.length > 50 ? description.slice(0, 50) + '...' : description}</p>
-      <h3 className="text-lg font-semibold mb-1">結末：</h3>
-      <p className="border-l-4 border-green-300 pl-4 text-gray-700 mb-4">{endingText}</p>
+      {endingImage && <img src={endingImage} alt="サムネイル" className="mx-auto mb-4" />}
+      <h3 className="text-xl md:text-xl lg:text-xl font-semibold text-center mb-4">{author}さんの悪夢</h3>
+      <p className="text-sm border-l-4 border-green-300 pl-4 text-gray-700 mb-4">{endingText}</p>
     </div>
   );
 };
