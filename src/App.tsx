b6e696f973532209beebcb8c4812a2912688431e
tokaisagami <tokai.sagami.54@gmail.com>
2025-01-16 22:47:55 +0900

    add:パスワードリセット画面ルート追加

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import HomePage from './components/HomePage/HomePage';
 import MyPage from './components/MyPage/MyPage';
 import AccountSettings from './components/AccountSettings/AccountSettings';
 import PasswordResetRequest from './components/PasswordResetRequest/PasswordResetRequest';
+import PasswordReset from './components/PasswordReset/PasswordReset';
 import { login } from './store/slices/authSlice';
 import { startLoading, stopLoading } from './store/slices/loadingSlice'; // ローディングアクションのインポート
 import { RootState } from './store/store'; // RootStateをインポート
@@ -29,6 +30,7 @@ const routes = [
   { path: '/mypage', element: <MyPage /> },
   { path: '/account-settings', element: <AccountSettings /> }, // 新しいルートの追加
   { path: '/password-reset-request', element: <PasswordResetRequest /> }, // 新しいルートの追加
+  { path: '/password-reset', element: <PasswordReset /> }, // 新しいルートの追加
 ];
 
 function App() {
